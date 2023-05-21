4c7e3aa3c4ab47192a823636f72ef2c98a2f1343
Esther Asare <97907344+Efisassy@users.noreply.github.com>
2023-05-21 09:56:18 +0000

    all files added

---
+++ script.js
@@ -0,0 +1,15 @@
+function fan(){
+    document.getElementById("me").innerHTML = "yes";
+}
+
+
+console.log(7+9)
+
+let texts= text;
+ function save(){
+    document.getElementById('display').innerHTML =text.value
+    
+ }
+ const d = new Date("October 13, 2014 11:13:00");
+document.getElementById('demo').innerHTML = d
+ 
